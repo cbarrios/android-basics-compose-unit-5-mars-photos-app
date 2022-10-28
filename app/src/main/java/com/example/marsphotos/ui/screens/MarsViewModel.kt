8442ebc7ce3f0936f83c434dba95279ede2f8628
cbarrios <lalobarrios16@yahoo.com>
2022-10-27 22:02:05 -0400

    master: (8) Parse the JSON response with kotlinx.serialization

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -49,7 +49,9 @@ class MarsViewModel : ViewModel() {
         viewModelScope.launch {
             marsUiState = try {
                 val listResult = MarsApi.retrofitService.getPhotos()
-                MarsUiState.Success(listResult)
+                MarsUiState.Success(
+                    "Success: ${listResult.size} Mars photos retrieved"
+                )
             } catch (e: IOException) {
                 MarsUiState.Error
             }
