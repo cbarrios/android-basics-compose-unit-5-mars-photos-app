e343051512c8f535b2a5bea22c18f377f5fbf607
cbarrios <lalobarrios16@yahoo.com>
2022-10-28 11:25:41 -0400

    master (3): (5) Update the ViewModel test

--- app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
+++ app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
@@ -17,7 +17,7 @@ class MarsViewModelTest {
     fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() = runTest {
         val marsViewModel = MarsViewModel(marsPhotosRepository = FakeNetworkMarsPhotosRepository())
         assertEquals(
-            MarsUiState.Success("Success: ${FakeDataSource.photosList.size} Mars photos retrieved"),
+            MarsUiState.Success(FakeDataSource.photosList),
             marsViewModel.marsUiState
         )
     }
